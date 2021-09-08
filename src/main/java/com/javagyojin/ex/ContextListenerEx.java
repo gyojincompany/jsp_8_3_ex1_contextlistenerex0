fc17474d88e6ceaf199de8e82b021ce682134492
ici-903-00 <ici-903-00@DESKTOP-9H8V5DQ>
2021-09-08 15:05:04 +0900

    컨텍스트 리스너 v1.0

---
+++ src/main/java/com/javagyojin/ex/ContextListenerEx.java
@@ -0,0 +1,48 @@
+package com.javagyojin.ex;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+
+/**
+ * Application Lifecycle Listener implementation class ContextListenerEx
+ *
+ */
+@WebListener
+public class ContextListenerEx implements ServletContextListener {
+
+    /**
+     * Default constructor. 
+     */
+    public ContextListenerEx() {
+    	
+        // TODO Auto-generated constructor stub
+    }
+       
+    /**
+     * @see ServletContextEvent#ServletContextEvent(ServletContext)
+     */
+    public ContextListenerEx(ServletContext source) {
+        // super(source);
+        // TODO Auto-generated constructor stub
+        
+    }
+
+	/**
+     * @see ServletContextListener#contextDestroyed(ServletContextEvent)
+     */
+    public void contextDestroyed(ServletContextEvent sce)  { 
+         // TODO Auto-generated method stub
+    	System.out.println("contextDestroyed 메서드가 실행되었습니다!!");
+    }
+
+	/**
+     * @see ServletContextListener#contextInitialized(ServletContextEvent)
+     */
+    public void contextInitialized(ServletContextEvent sce)  { 
+         // TODO Auto-generated method stub
+    	System.out.println("contextInitialized 메서드가 실행되었습니다!!");
+    }
+	
+}
